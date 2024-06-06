82e3dcef356e4aa58f878cf26925e7df92cdf2e0
Muhammadzoir Mahmudov <kolen1470@gmail.com>
2024-06-06 23:08:07 +0500

    third steep

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,9 +13,14 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Head>
+        <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
       <body className={inter.className}>
-        <Navbar/>
-        {children}
+        <Navbar />
+        <main>{children}</main>
       </body>
     </html>
   );
